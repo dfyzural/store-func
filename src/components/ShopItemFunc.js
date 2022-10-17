efd580a63b0a46314db9f26055bd47afaf32566a
dfyzural <dfyz.ural@gmail.com>
2022-10-17 15:23:06 +0500

    propTypes add

--- src/components/ShopItemFunc.js
+++ src/components/ShopItemFunc.js
@@ -1,22 +1,28 @@
+import PropTypes from 'prop-types';
+
 function ShopItemFunc(props) {
     const { item } = props;
 
     return (
-        <div class="main-content">
+        <div className="main-content">
             <h2>{item.brand}</h2>
             <h1>{item.title}</h1>
             <h3>{item.description}</h3>
-            <div class="description">
+            <div className="description">
                 {item.descriptionFull}
             </div>
-            <div class="highlight-window mobile"><div class="highlight-overlay"></div></div>
-            <div class="divider"></div>
-            <div class="purchase-info">
-                <div class="price">{item.currency+item.price.toFixed(2)}</div>
+            <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+            <div className="divider"></div>
+            <div className="purchase-info">
+                <div className="price">{item.currency+item.price.toFixed(2)}</div>
                 <button>Добавить в корзину</button>
             </div>
         </div>
     );
+}
+
+ShopItemFunc.propTypes = {
+    item: PropTypes.object.isRequired
 }
 
 export default ShopItemFunc;
